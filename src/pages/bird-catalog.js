c005ea3f5d3c1197c6fb3c49e8bbdb45938748e0
j1mmyzz <jime82075@gmail.com>
2023-06-17 18:41:52 -0400

    bird 2.0

---
+++ src/pages/bird-catalog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BirdCatalog() {
+  return (
+    <div>
+      <h1>Bird Catalog</h1>
+      <h1>Bird Catalog</h1>
+      <h1>Bird Catalog</h1>
+      <h1>Bird Catalog</h1>
+    </div>
+  );
+}
+
+export default BirdCatalog;
